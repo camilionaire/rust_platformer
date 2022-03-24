59a1e6c988e848d3b4efb60d8f74f1683f7d8f77
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-03-23 18:12:00 -0700

    a small example.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,28 @@ fn hello_world() {
     println!("hello world")
 }
 
+struct Person;
+struct Name(String);
+
 fn main() {
     App::build()
+        .add_startup_system(add_people.system())
         .add_system(hello_world.system())
+        .add_system(greet_people.system())
         .run();
     println!("Something happened!...");
+}
+
+// a system
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Camilo Hughes".to_string()));
+    commands.spawn().insert(Person).insert(Name("Helena Bobena".to_string()));
+    commands.spawn().insert(Person).insert(Name("Jon Stevens".to_string()));
+}
+
+// another system
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
 }
